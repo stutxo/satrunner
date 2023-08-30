6a3a895409d198183050d6423a6deacb310dac9d
stum0 <70952638+stum0@users.noreply.github.com>
2023-08-30 10:48:20 -0400

    fix release build

--- src/network/websockets.rs
+++ src/network/websockets.rs
@@ -14,8 +14,8 @@ use super::messages::ClientMessage;
 // use gloo_timers::future::TimeoutFuture;
 
 pub fn websocket(mut network_stuff: ResMut<NetworkStuff>, mut ping: ResMut<PingTimer>) {
-    let ws = WebSocket::open("ws://0.0.0.0:3030/run").unwrap();
-    //let ws = WebSocket::open("wss://satrunner.gg/run").unwrap();
+    //let ws = WebSocket::open("ws://0.0.0.0:3030/run").unwrap();
+    let ws = WebSocket::open("wss://satrunner.gg/run").unwrap();
     let (mut write, mut read) = ws.split();
 
     let (send_tx, mut send_rx) = futures::channel::mpsc::channel::<ClientMessage>(1000);
