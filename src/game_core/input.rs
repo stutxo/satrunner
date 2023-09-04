c03cc5556a22543415436df41a3e5c2e9e2947ff
stum0 <70952638+stum0@users.noreply.github.com>
2023-09-04 17:11:07 -0400

    edit speed 3

--- src/game_core/input.rs
+++ src/game_core/input.rs
@@ -18,64 +18,64 @@ pub fn input(
     mut outgoing: ResMut<NetworkStuff>,
     client_tick: Res<ClientTick>,
 ) {
-    // for mut player in query.iter_mut() {
-    //     let (camera, camera_transform) = camera_query.single();
+    for mut player in query.iter_mut() {
+        let (camera, camera_transform) = camera_query.single();
 
-    //     let get_position = |cursor_position: Vec2, window: &Window| {
-    //         let screen_size = Vec2::new(window.width(), window.height());
-    //         let screen_position = Vec2::new(
-    //             cursor_position.x / screen_size.x,
-    //             1.0 - (cursor_position.y / screen_size.y),
-    //         );
+        let get_position = |cursor_position: Vec2, window: &Window| {
+            let screen_size = Vec2::new(window.width(), window.height());
+            let screen_position = Vec2::new(
+                cursor_position.x / screen_size.x,
+                1.0 - (cursor_position.y / screen_size.y),
+            );
 
-    //         let clip_position = (screen_position - Vec2::new(0.5, 0.5)) * 2.0;
-    //         let mut position = camera
-    //             .projection_matrix()
-    //             .inverse()
-    //             .project_point3(clip_position.extend(0.0));
-    //         position = *camera_transform * position;
-    //         position.truncate()
-    //     };
+            let clip_position = (screen_position - Vec2::new(0.5, 0.5)) * 2.0;
+            let mut position = camera
+                .projection_matrix()
+                .inverse()
+                .project_point3(clip_position.extend(0.0));
+            position = *camera_transform * position;
+            position.truncate()
+        };
 
-    //     if client_tick.pause == 0 {
-    //         let mut handle_input = |cursor_position: Vec2, player: &mut Player| {
-    //             player.target = cursor_position;
+        if client_tick.pause == 0 {
+            let mut handle_input = |cursor_position: Vec2, player: &mut Player| {
+                player.target = cursor_position;
 
-    //             let input = PlayerInput::new(
-    //                 [player.target.x, player.target.y],
-    //                 player.id,
-    //                 client_tick.tick.unwrap(),
-    //                 true,
-    //             );
+                let input = PlayerInput::new(
+                    [player.target.x, player.target.y],
+                    player.id,
+                    client_tick.tick.unwrap(),
+                    true,
+                );
 
-    //             player.pending_inputs.push(input.clone());
+                player.pending_inputs.push(input.clone());
 
-    //             // info!(
-    //             //     "Sending input: {:?}, player pos: {:?}",
-    //             //     input, t.translation.x
-    //             // );
+                // info!(
+                //     "Sending input: {:?}, player pos: {:?}",
+                //     input, t.translation.x
+                // );
 
-    //             match outgoing
-    //                 .write
-    //                 .as_mut()
-    //                 .unwrap()
-    //                 .try_send(ClientMessage::PlayerInput(input))
-    //             {
-    //                 Ok(()) => {}
-    //                 Err(e) => error!("Error sending message: {} CHANNEL FULL???", e),
-    //             };
-    //         };
+                match outgoing
+                    .write
+                    .as_mut()
+                    .unwrap()
+                    .try_send(ClientMessage::PlayerInput(input))
+                {
+                    Ok(()) => {}
+                    Err(e) => error!("Error sending message: {} CHANNEL FULL???", e),
+                };
+            };
 
-    //         if mouse.just_pressed(MouseButton::Left) || mouse.just_pressed(MouseButton::Right) {
-    //             if let Some(window) = windows.iter().next() {
-    //                 if let Some(cursor) = window.cursor_position() {
-    //                     let position = get_position(cursor, window);
-    //                     handle_input(position, &mut player);
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
+            if mouse.just_pressed(MouseButton::Left) || mouse.just_pressed(MouseButton::Right) {
+                if let Some(window) = windows.iter().next() {
+                    if let Some(cursor) = window.cursor_position() {
+                        let position = get_position(cursor, window);
+                        handle_input(position, &mut player);
+                    }
+                }
+            }
+        }
+    }
 }
 
 pub fn update_joystick(
